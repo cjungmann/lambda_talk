8465c0fc6576cd994eededd98cec8c7c8def068d
--global <--global>
2017-09-05 16:00:14 -0500

    Reorder and add some comment text.

--- 03_qsort/qsort.cpp
+++ 03_qsort/qsort.cpp
@@ -6,13 +6,6 @@
 
 int comps_made;
 
-int compar(const void* left, const void* right)
-{
-   ++comps_made;
-   return strcasecmp(*static_cast<const char* const*>(left),
-                     *static_cast<const char* const*>(right));
-}
-
 const char** copy_array(const char**target, const char* const* source, int count)
 {
    for (int i=0; i<count; ++i)
@@ -32,23 +25,38 @@ void print_array(const char**array, int count)
    putchar('\n');
 }
 
-void sort(const char* const* strings, int count)
+/** Note how compar() sets global varialbe comps_made. */
+int compar(const void* left, const void* right)
 {
-   // Make copy of strings array
-   const char **arr = copy_array(new const char*[count], strings, count);
+   ++comps_made;
+   return strcasecmp(*static_cast<const char* const*>(left),
+                     *static_cast<const char* const*>(right));
+}
 
-   qsort(arr, count, sizeof(char*), compar);
+void sort(const char** strings, int count)
+{
+   qsort(strings, count, sizeof(char*), compar);
 
    puts("Sentence after sorting:\n");
-   print_array(arr, count);
+   print_array(strings, count);
 
    printf("\n%d comparisons made to sort %d elements.\n", comps_made, count);
-
-   delete [] arr;
 }
 
 int main(int argc, char** argv)
 {
-   sort(&argv[1], argc-1);
+   if (argc < 2)
+   {
+      printf("Please enter a list of words after qsort command name.\n\n");
+      return 1;
+   }
+
+   // Make copy of strings array
+   const char **arr = copy_array(new const char*[argc-1], &argv[1], argc-1);
+
+   sort(arr, argc-1);
+
+   delete [] arr;
+
    return 0;
 }
